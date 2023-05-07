64bdf2bfd54dade0ecbd86d2a7fb3167f554ee73
Aliaksei Krutsko <wonder4ik@gmail.com>
2023-05-07 11:15:16 +0300

    refactor: use OS-specific end-of-line marker

--- 02-write-file/index.js
+++ 02-write-file/index.js
@@ -1,6 +1,7 @@
 const path = require('path');
 const fs = require('fs');
 const readline = require('readline');
+const os = require('os');
 
 const pathToFile = path.join(__dirname, 'output.txt');
 const file = fs.createWriteStream(pathToFile, 'utf-8');
@@ -8,10 +9,9 @@ const file = fs.createWriteStream(pathToFile, 'utf-8');
 // clear console
 process.stdout.write('\033c');
 
-// const { stdin: input, stdout: output } = process;
 const rl = readline.createInterface(process.stdin, process.stdout);
 
-rl.write('Please enter some text:\n');
+rl.write(`Please enter some text:${os.EOL}`);
 
-rl.on('line', input => input === 'exit' ? rl.close() : file.write(`${input}\n`));
+rl.on('line', input => input === 'exit' ? rl.close() : file.write(`${input}${os.EOL}`));
 rl.on('close', () => process.stdout.write('Bye!'));
