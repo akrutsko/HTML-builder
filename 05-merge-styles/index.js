b04a067c11164042bf73d3d967727059772ed469
Aliaksei Krutsko <wonder4ik@gmail.com>
2023-05-14 23:21:54 +0300

    refactor: improve performance and stability

--- 05-merge-styles/index.js
+++ 05-merge-styles/index.js
@@ -1,26 +1,27 @@
 const path = require('path');
 const fs = require('fs');
+const fsPromises = require('fs/promises');
+const { pipeline } = require('stream/promises');
 
 const sourceDir = path.join(__dirname, 'styles');
 const destinationDir = path.join(__dirname, 'project-dist');
 const bundleFile = path.join(destinationDir, 'bundle.css');
 
-fs.writeFile(bundleFile, '', err => {
-  if (err) throw err;
+async function createBundle() {
+  await fsPromises.rm(bundleFile, { force: true });
 
-  fs.readdir(sourceDir, { withFileTypes: true }, (err, files) => {
-    if (err) throw err;
+  const files = await fsPromises.readdir(sourceDir, { withFileTypes: true });
+  const cssFiles = files.filter((file) => path.extname(file.name) === '.css');
 
-    const cssFiles = files.filter(file => path.extname(file.name) === '.css');
-
-    cssFiles.forEach(file => {
-      fs.readFile(path.join(sourceDir, file.name), (err, data) => {
-        if (err) throw err;
-
-        fs.appendFile(bundleFile, data, err => {
-          if (err) throw err;
-        });
-      });
+  for (const file of cssFiles) {
+    const sourceFile = path.join(sourceDir, file.name);
+    const rs = fs.createReadStream(sourceFile, 'utf-8');
+    const ws = fs.createWriteStream(bundleFile, {
+      encoding: 'utf-8',
+      flags: 'a',
     });
-  });
-});
+    await pipeline(rs, ws);
+  }
+}
+
+createBundle();
